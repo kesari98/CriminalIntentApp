d11dc472f022e63e5883b75314b6e28d79398bec
kesari98 <pkkesari14@gmail.com>
2024-01-03 15:53:09 +0530

    using only one view Type as per project requirement

--- CriminalIntentApp/app/src/main/java/com/example/criminalintentapp/CrimeListAdapter.kt
+++ CriminalIntentApp/app/src/main/java/com/example/criminalintentapp/CrimeListAdapter.kt
@@ -6,48 +6,22 @@ import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.example.criminalintentapp.databinding.ListItemCrimeBinding
-import com.example.criminalintentapp.databinding.ListItemCrimePoliceBinding
 
-class CrimeListAdapter(private val crimes: List<Crime>) : RecyclerView.Adapter<ViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+class CrimeListAdapter(private val crimes: List<Crime>) : RecyclerView.Adapter<CrimeHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CrimeHolder {
         val inflater = LayoutInflater.from(parent.context)
+        val binding = ListItemCrimeBinding.inflate(inflater, parent, false)
+        return CrimeHolder(binding)
+    }
 
-        return if (viewType == 1) {
-            val binding = ListItemCrimePoliceBinding.inflate(inflater, parent, false)
-            CrimePoliceHolder(binding)
-        } else {
-            val binding = ListItemCrimeBinding.inflate(inflater, parent, false)
-            CrimeHolder(binding)
-        }
-
+    override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
+        val crime = crimes[position]
+        holder.bind(crime)
     }
 
 
     override fun getItemCount(): Int {
         return crimes.size
-    }
-
-    override fun getItemViewType(position: Int): Int {
-        return if(crimes[position].requiresPolice) {
-            1
-        } else {
-            0
-        }
-    }
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val crime = crimes[position]
-        when (holder.itemViewType) {
-            1 -> {
-                val policeHolder = holder as CrimePoliceHolder
-                policeHolder.bind(crime)
-            }
-
-            0 -> {
-                val crimeHolder = holder as CrimeHolder
-                crimeHolder.bind(crime)
-            }
-        }
     }
 }
 
@@ -68,26 +42,3 @@ class CrimeHolder(
     }
 }
 
-class CrimePoliceHolder(
-    private val binding: ListItemCrimePoliceBinding
-) : ViewHolder(binding.root) {
-    fun bind(crime: Crime) {
-        binding.crimeTitle.text = crime.title
-        binding.crimeDate.text = crime.date.toString()
-
-        binding.root.setOnClickListener {
-            Toast.makeText(
-                binding.root.context,
-                "${crime.title} clicked!",
-                Toast.LENGTH_SHORT
-            ).show()
-        }
-        binding.policeButton.setOnClickListener {
-            Toast.makeText(
-                binding.root.context,
-                "Police Contacted",
-                Toast.LENGTH_SHORT
-            ).show()
-        }
-    }
-}
