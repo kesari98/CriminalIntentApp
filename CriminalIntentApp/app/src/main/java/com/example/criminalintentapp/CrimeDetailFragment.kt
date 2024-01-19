a12a5c75dd051dc04b131366d0c8c823236fa4e7
kesari98 <pkkesari14@gmail.com>
2024-01-19 17:18:53 +0530

    Using onBackPressedCallback to handle the case when the user makes title as empty

--- CriminalIntentApp/app/src/main/java/com/example/criminalintentapp/CrimeDetailFragment.kt
+++ CriminalIntentApp/app/src/main/java/com/example/criminalintentapp/CrimeDetailFragment.kt
@@ -1,22 +1,21 @@
 package com.example.criminalintentapp
 
 import android.os.Bundle
-import android.text.format.DateFormat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.criminalintentapp.databinding.FragmentCrimeDetailBinding
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
-import java.util.Date
-import java.util.UUID
 
 class CrimeDetailFragment: Fragment() {
     private  var _binding: FragmentCrimeDetailBinding? =null
@@ -58,6 +57,18 @@ class CrimeDetailFragment: Fragment() {
                     oldCrime.copy(isSolved = isChecked)
                 }
             }
+
+            requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner,object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    if(crimeTitle.text.toString() != "") {
+                        findNavController().popBackStack()
+                    }
+                    if(crimeTitle.text.toString() == "") {
+                        Toast.makeText(context,"Title is empty",Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+            })
         }
 
         viewLifecycleOwner.lifecycleScope.launch {
